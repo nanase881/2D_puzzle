7db387c43e5f316fcc0a63f21ff24fc30a545c48
nanase881 <s1290002@u-aizu.ac.jp>
2023-11-13 10:36:38 +0900

    Implement game over

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,26 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    Rigidbody2D rb;
     public bool falled = false;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+
     }
 
     private void Update()
     {
-        //if (!gameObject.CompareTag("operatable"))
-        //{
-        //    if (rb.velocity.x < 0.1f && rb.velocity.y < 0.1f)
-        //    {
-                
-        //    }
-        //}
+        if(falled == false)
+        {
+            CancelInvoke("Result");
+        }
         
     }
 
@@ -29,21 +26,26 @@ public class GameOver : MonoBehaviour
         falled = true;
     }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        falled = false;
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Spawn"))
         {
             if (falled == true)
             {
-                Invoke("Result", 5f);
-                 // 途中でfalledがfalseになったら、Involke処理をキャンセル出来るようにする
+                Invoke("Result",  5f);
             }
         }
     }
 
     void Result()
     {
-        Debug.Log("Game over");
+        //Debug.Log("Game over");
+        SceneManager.LoadScene("Result");
     }
 
 }
